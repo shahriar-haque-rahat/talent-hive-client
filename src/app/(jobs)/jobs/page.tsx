b5c4a4403fd6d044923387537077f40b96d806de
shRahat10 <shahriar.haque.1011@gmail.com>
2024-08-24 01:24:32 +0600

    new route

---
+++ src/app/(jobs)/jobs/page.tsx
@@ -0,0 +1,12 @@
+import Jobs from '@/components/jobs/Jobs';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <Jobs/>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/(notification)/notification/page.tsx
@@ -0,0 +1,12 @@
+import Notification from '@/components/notification/Notification';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <Notification/>
+        </div>
+    );
+};
+
+export default page;
